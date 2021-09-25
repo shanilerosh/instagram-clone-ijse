5cc470b7180e0a0fb94e1000cff10b9e6c45b45c
shanil <shanilmiranda24@gmail.com>
2021-09-25 09:07:33 +0530

    added bottom navigation for the main components

--- components/MainComp.js
+++ components/MainComp.js
@@ -4,6 +4,10 @@ import {bindActionCreators} from "redux";
 import {fetchUser} from "../redux-related/redux_actions";
 import {Text, View} from "react-native";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import Feed from "./main-screens/feed";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import AddScreen from "./main-screens/AddScreen";
+import ProfileScreen from "./main-screens/ProfileScreen";
 
 /*Initializing the bottom tab navigatror*/
 const Tab = createBottomTabNavigator();
@@ -19,9 +23,32 @@ class MainComp extends Component {
     render() {
         const {currUser} = this.props;
         console.log('Curr User ===>', currUser)
+
+
+
         return (
             <Tab.Navigator>
-                <Tab.Screen name={"Feeds" }  />
+                 <Tab.Screen name={"Feed"}  component={Feed}
+                    options={{
+                        tabBarIcon: ({color, size}) => (
+                            <MaterialCommunityIcons name="home" color = {color}
+                            size={26} />
+                        ),
+                    }}/>
+                <Tab.Screen name={"Add"}  component={AddScreen}
+                            options={{
+                                tabBarIcon: ({color, size}) => (
+                                    <MaterialCommunityIcons name="plus-box" color = {color}
+                                                            size={26} />
+                                ),
+                            }}/>
+                <Tab.Screen name={"Profile"}  component={ProfileScreen}
+                            options={{
+                                tabBarIcon: ({color, size}) => (
+                                    <MaterialCommunityIcons name="account-circle" color = {color}
+                                                            size={26} />
+                                ),
+                            }}/>
             </Tab.Navigator>
         );
     }
---
+++ components/main-screens/AddScreen.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, View} from "react-native";
+
+class Feed extends Component {
+    render() {
+        return (
+           <View>
+               <Text>Feed</Text>
+           </View>
+        );
+    }
+}
+
+export default Feed;
